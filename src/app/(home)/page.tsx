4574a58b88ab3161970d6b26a985f7475edb44c3
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-24 18:24:59 -0300

    feat(home): fix h1 padding

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   const todoList = await prisma.todo.findMany();
 
   return (
-    <main className="flex min-h-screen container flex-col items-center justify-between px-24 py-8">
+    <main className="flex min-h-screen container flex-col items-center justify-between px-24 py-4">
       <section className="w-3/4">
         <div className="mx-2 my-2 bg-slate-100 rounded-sm dark:bg-transparent grid grid-cols-1 gap-4 p-5 ">
           <h1 className="text-5xl text-center"> TODO LIST</h1>
