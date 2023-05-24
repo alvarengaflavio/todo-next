979fbe13dd15f499bb87536b7fd29422b6224b3d
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-24 18:24:13 -0300

    feat(home): fix h1 position

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -9,9 +9,8 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen container flex-col items-center justify-between px-24 py-8">
       <section className="w-3/4">
-        <h1 className="text-5xl text-center"> TODO LIST</h1>
-
         <div className="mx-2 my-2 bg-slate-100 rounded-sm dark:bg-transparent grid grid-cols-1 gap-4 p-5 ">
+          <h1 className="text-5xl text-center"> TODO LIST</h1>
           {todoList.map((todo) => (
             <Suspense key={todo.id} fallback={<TodoSkeleton />}>
               {/* @ts-expect-error Async Server Component */}
