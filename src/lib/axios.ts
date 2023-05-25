dfb6e2610ee0f164d70aeea4bb40cd7b297333d7
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-25 16:35:13 -0300

    feat(lib): create new axios instance

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,28 +1,10 @@
-import { Todo } from "@/types";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
-  timeout: 1000,
-  headers: { "X-Custom-Header": "foobar" },
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
-
-export const getTodos = async () => {
-  try {
-    const response = await api.get("/api/todo");
-    return response.data;
-  } catch (error) {
-    return { error };
-  }
-};
-
-export const postTodo = async (todo: Todo) => {
-  try {
-    const response = await api.post("/api/todo", todo);
-    return response.data;
-  } catch (error) {
-    return { error };
-  }
-};
