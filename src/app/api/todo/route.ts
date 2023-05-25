a8b974bc530ad3b9f3482f0467a93b58ec7f8936
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-25 16:54:55 -0300

    feat(api): ordered `Todo` in get all

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -1,14 +1,24 @@
 import { prisma } from "@/lib/db";
 import { exceptionHandler } from "@/lib/exception-handler";
+import { Prisma } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   try {
-    const { searchParams } = new URL(req.url);
-    const mySearchParam = searchParams.get("search");
+    // const { searchParams } = new URL(req.url);
+    // const mySearchParam = searchParams.get("search");
+    const orderBy: Prisma.Enumerable<Prisma.TodoOrderByWithRelationInput> = [
+      {
+        createdAt: "desc",
+      },
+      {
+        done: "asc",
+      },
+    ];
 
-    // console.log(mySearchParam);
-    const todoList = await prisma.todo.findMany();
+    const todoList = await prisma.todo.findMany({
+      orderBy: orderBy,
+    });
 
     return NextResponse.json(todoList, {
       status: 200,
@@ -22,11 +32,9 @@ export async function GET(req: NextRequest) {
 export async function POST(req: NextRequest) {
   try {
     const data = await req.json(); // data is a JS object with the JSON-parsed body
-    const newTodo = await prisma.todo.create({
-      data,
-    });
+    const newTodo = await prisma.todo.create({ data });
 
-    return new Response(JSON.stringify(newTodo), {
+    return NextResponse.json(newTodo, {
       status: 201,
       statusText: "OK",
     });
