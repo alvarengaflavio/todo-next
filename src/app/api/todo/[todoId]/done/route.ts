e5ef07b67c9a7ca9a5e975215436b2c0dcf44904
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-26 10:52:30 -0300

    feat(api): create `todoId` route

---
+++ src/app/api/todo/[todoId]/done/route.ts
@@ -0,0 +1,35 @@
+import { prisma } from "@/lib/db";
+import { exceptionHandler } from "@/lib/exception-handler";
+import { BadRequestException } from "@/lib/exceptions";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(req: NextRequest, context: Context) {
+  try {
+    const { params } = context;
+    const body = await req.json();
+
+    if (!params.todoId) throw new BadRequestException("ID não informado");
+    if (typeof body.done !== "boolean")
+      throw new BadRequestException("Done não informado");
+
+    const data: DonePayload = { done: body.done };
+    const where = { id: params.todoId };
+    const updatedTodo = await prisma.todo.update({ where, data });
+
+    return NextResponse.json(updatedTodo, {
+      status: 200,
+    });
+  } catch (error: any) {
+    return exceptionHandler(error);
+  }
+}
+
+type Context = {
+  params: {
+    todoId: string;
+  };
+};
+
+type DonePayload = {
+  done: boolean;
+};
