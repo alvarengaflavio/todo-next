2fb18de73e54426ba932692a6dbb593f6076cb69
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-27 11:29:54 -0300

    feat(todo-id): add todo card

--- src/app/todo/[todoId]/page.tsx
+++ src/app/todo/[todoId]/page.tsx
@@ -1,4 +1,14 @@
+import { Button, buttonVariants } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
 import { getTodo } from "@/lib/axios-instance";
+import { getDateToLocale } from "@/lib/utils";
+import Link from "next/link";
 
 interface PageProps {
   params: {
@@ -6,25 +16,43 @@ interface PageProps {
   };
 }
 
-const TodoId = async ({ params }: PageProps) => {
+const TodoPage = async ({ params }: PageProps) => {
   const todo = await getTodo(params.todoId);
-  console.log(todo);
 
   return (
-    <div className="text-center">
-      <h2>Todo ID</h2>
-      <span>{params.todoId}</span>
-      <h2>Todo</h2>
-      <div className="flex flex-col">
-        <span>{todo.title}</span>
-        <span>{todo.done}</span>
-        <span>{todo.createdAt}</span>
+    <div className="text-center container flex flex-col items-center">
+      <Card className="w-3/4 p-4 my-4 dark:shadow-foreground/10 shadow-md ">
+        <CardHeader className="text-slate-400">TAREFA</CardHeader>
+        <CardContent className="text-4xl">
+          <span>{todo.title}</span>
+        </CardContent>
+
+        <CardDescription className="text-xl">
+          <span>{todo.done ? `Completa` : `Incompleta`}</span>
+        </CardDescription>
+
+        <CardFooter className="text-center text-lg text-slate-500">
+          <span className="min-w-full">
+            {todo.done
+              ? `Finalizada em ${getDateToLocale(todo.updatedAt)}`
+              : `Criada em ${getDateToLocale(todo.createdAt)}`}
+          </span>
+        </CardFooter>
+      </Card>
+
+      <div className="text-4xl w-3/4 font-bold my-2 flex justify-between gap-2">
+        <Link href="/" className={buttonVariants({ variant: "outline" })}>
+          Voltar
+        </Link>
+
+        <div className="flex gap-x-2">
+          <Button variant={"default"}>Editar</Button>
+
+          <Button variant={"destructive"}>Excluir</Button>
+        </div>
       </div>
     </div>
   );
 };
 
-export default TodoId;
-function getTodoById(todoId: string) {
-  throw new Error("Function not implemented.");
-}
+export default TodoPage;
