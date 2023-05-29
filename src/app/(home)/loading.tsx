3447b262caf5a80743cd9e0da4dc389dc442b353
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-29 10:47:40 -0300

    feat(home): create `loading` page

---
+++ src/app/(home)/loading.tsx
@@ -0,0 +1,29 @@
+import { ToastSimple } from "@/components/toast-simple";
+import { CreateTodoForm } from "@/components/todo-create";
+import { TodoSkeleton } from "@/components/todo-skeleton";
+
+export default async function LoadingHome() {
+  return (
+    <main className="flex min-h-screen container flex-col items-center justify-between px-24 py-4">
+      <section className="w-3/4">
+        <div className="mx-2 my-2 bg-slate-100 rounded-sm dark:bg-transparent grid grid-cols-1 gap-4 p-5 ">
+          <h1 className="text-5xl text-center font-light"> TODO LIST</h1>
+          <CreateTodoForm />
+
+          <ul className="space-y-4">
+            <li>
+              <TodoSkeleton />
+            </li>
+            <li>
+              <TodoSkeleton />
+            </li>
+            <li>
+              <TodoSkeleton />
+            </li>
+          </ul>
+        </div>
+        <ToastSimple />
+      </section>
+    </main>
+  );
+}
