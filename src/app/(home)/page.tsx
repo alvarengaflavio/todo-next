30c17e4a829b35a7a3594d2fbc5b2b1b1c885318
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-29 07:47:04 -0300

    feat(home): add `CreateTodoForm`

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,6 @@
 import { ToastSimple } from "@/components/toast-simple";
 import TodoCard from "@/components/todo-card";
+import { CreateTodoForm } from "@/components/todo-create";
 import { TodoSkeleton } from "@/components/todo-skeleton";
 import { getTodos } from "@/lib/axios-instance";
 
@@ -12,7 +13,8 @@ export default async function Home() {
     <main className="flex min-h-screen container flex-col items-center justify-between px-24 py-4">
       <section className="w-3/4">
         <div className="mx-2 my-2 bg-slate-100 rounded-sm dark:bg-transparent grid grid-cols-1 gap-4 p-5 ">
-          <h1 className="text-5xl text-center"> TODO LIST</h1>
+          <h1 className="text-5xl text-center font-light"> TODO LIST</h1>
+          <CreateTodoForm />
           {todoList.map((todo) => (
             <Suspense key={todo.id} fallback={<TodoSkeleton />}>
               {/* @ts-expect-error Async Server Component */}
