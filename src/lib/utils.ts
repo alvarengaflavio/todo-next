9e58274eee28e0b3fd95381e02df490dd507eeb6
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-29 10:55:34 -0300

    feat(lib): add `sortTodoList` function & change `getDateLocale`

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+import type { Todo } from "@/types";
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -10,5 +11,18 @@ export function getDateToLocale(date: number | string | Date) {
     day: "2-digit",
     month: "long",
     year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+    dayPeriod: "short",
+  });
+}
+
+export function sortTodoList(todos: Todo[]) {
+  return todos.sort((a, b) => {
+    if (a.done === b.done) {
+      return a.createdAt > b.createdAt ? -1 : 1;
+    }
+    return a.done ? 1 : -1;
   });
 }
