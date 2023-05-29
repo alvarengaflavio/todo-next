c91062618bf4e4ecccbfb5620fee5beb3198f834
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-29 07:47:23 -0300

    feat(api): create DELETE todo route

--- src/app/api/todo/[todoId]/route.ts
+++ src/app/api/todo/[todoId]/route.ts
@@ -3,6 +3,12 @@ import { exceptionHandler } from "@/lib/exception-handler";
 import { BadRequestException, NotFoundException } from "@/lib/exceptions";
 import { NextRequest, NextResponse } from "next/server";
 
+type Context = {
+  params: {
+    todoId: string;
+  };
+};
+
 export async function PATCH(req: NextRequest, context: Context) {
   try {
     const title = await req.json(); // data is a JS object with the JSON-parsed body
@@ -40,8 +46,19 @@ export async function GET(req: NextRequest, context: Context) {
   }
 }
 
-type Context = {
-  params: {
-    todoId: string;
-  };
-};
+export async function DELETE(req: NextRequest, context: Context) {
+  try {
+    const { params } = context;
+
+    if (!params.todoId) throw new BadRequestException("ID não informado");
+
+    const where = { id: params.todoId };
+    const todo = await prisma.todo.delete({ where });
+
+    if (!todo) throw new NotFoundException("Todo não encontrado");
+
+    return NextResponse.json(todo, { status: 200 });
+  } catch (error: any) {
+    return exceptionHandler(error);
+  }
+}
