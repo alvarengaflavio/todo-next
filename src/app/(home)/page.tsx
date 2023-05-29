d9f60eacb55b60939c9fb3b90fe8852e3aaa976c
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-29 10:48:37 -0300

    feat(home): change `Suspense` strategy for `state`

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,7 @@
 import { ToastSimple } from "@/components/toast-simple";
-import TodoCard from "@/components/todo-card";
 import { CreateTodoForm } from "@/components/todo-create";
-import { TodoSkeleton } from "@/components/todo-skeleton";
+import { TodoList } from "@/components/todo-list";
 import { getTodos } from "@/lib/axios-instance";
-
-import { Suspense } from "react";
 
 export default async function Home() {
   const todoList = await getTodos();
@@ -12,15 +9,10 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen container flex-col items-center justify-between px-24 py-4">
       <section className="w-3/4">
-        <div className="mx-2 my-2 bg-slate-100 rounded-sm dark:bg-transparent grid grid-cols-1 gap-4 p-5 ">
+        <div className="mx-2 my-2 bg-slate-100 rounded-sm dark:bg-transparent grid grid-cols-1 gap-4 p-5">
           <h1 className="text-5xl text-center font-light"> TODO LIST</h1>
           <CreateTodoForm />
-          {todoList.map((todo) => (
-            <Suspense key={todo.id} fallback={<TodoSkeleton />}>
-              {/* @ts-expect-error Async Server Component */}
-              <TodoCard key={todo.id} todo={todo} />
-            </Suspense>
-          ))}
+          <TodoList todos={todoList} />
         </div>
         <ToastSimple />
       </section>
