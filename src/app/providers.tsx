12d32cc66887f112c5dfc8728d83bb48948b8cb7
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-30 16:02:09 -0300

    feat(providers): add `TodoContextProvider`

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { CounterContextProvider } from "@/context/counter.context";
+import { TodoContextProvider } from "@/context/todo-context";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider {...props}>
-      <CounterContextProvider>{children}</CounterContextProvider>
+      <TodoContextProvider>{children}</TodoContextProvider>
     </NextThemesProvider>
   );
 }
