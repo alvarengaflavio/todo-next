d222a5a1a99373ecdc0ff9009ef16b6b9be4dfa2
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-30 12:04:20 -0300

    feat(context): add ContextProvider inside providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import { CounterContextProvider } from "@/context/counter.context";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    <NextThemesProvider {...props}>
+      <CounterContextProvider>{children}</CounterContextProvider>
+    </NextThemesProvider>
+  );
 }
