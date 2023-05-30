87e3b8281c4a75138b32d291b8df67595e0a2c7e
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-30 16:08:45 -0300

    feat(providers): remove old imports

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { CounterContextProvider } from "@/context/counter.context";
 import { TodoContextProvider } from "@/context/todo-context";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
