29e3f7fb13605f629715dc893de6746543584634
Flavio Alvarenga <flavio.alva@outlook.com>
2023-05-30 16:04:15 -0300

    feat(todo): create todo-reducer

--- src/context/todo-reducer.tsx
+++ src/context/todo-reducer.tsx
@@ -1,23 +1,34 @@
 import { Todo } from "@/types";
 import { StateType } from "./todo-context";
+import { sortTodoList } from "@/lib/utils";
 
 export type ActionType = {
   type: string;
-  todos?: Todo[];
-  id?: string;
+  payload?: Todo[] | string;
 };
 
 export const reducer = (state: StateType, action: ActionType) => {
   switch (action.type) {
-    case "SET":
-      if (!action.todos) return state;
-      return { ...state, todos: [...action.todos] };
+    case "SET_LIST":
+      if (!(action.payload instanceof Array)) return state;
+      return { ...state, todos: [...action.payload] };
 
-    case "UPDATE":
-      if (!action.todos) return state;
-      return { ...state, todos: [...state.todos, ...action.todos] };
+    case "UPDATE_LIST":
+      if (!(action.payload instanceof Array)) return state;
+      return { ...state, todos: [...state.todos, ...action.payload] };
 
-    case "CHECK":
+    case "SET_DONE":
+      if (!(typeof action.payload === "string")) return state;
+
+      state.todos.forEach((todo) => {
+        if (todo.id === action.payload) {
+          todo.done = !todo.done;
+        }
+      });
+
+      sortTodoList(state.todos);
+      console.log("SET_DONE", state.todos);
+
       return { ...state, todos: [...state.todos] };
 
     default:
