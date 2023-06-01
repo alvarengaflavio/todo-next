35ce57f3238eed034b495cc7f1b5a10d08d1344e
Flavio Alvarenga <flavio.alva@outlook.com>
2023-06-01 18:50:33 -0300

    feat(toast-destructive): add toast action

--- src/components/toast-destructive.tsx
+++ src/components/toast-destructive.tsx
@@ -3,30 +3,37 @@
 import { Button } from "@/components/ui/button";
 import { ToastAction } from "@/components/ui/toast";
 import { useToast } from "@/components/ui/use-toast";
+import { ToastClose } from "@radix-ui/react-toast";
 
 export function ToastDestructive() {
   const { toast } = useToast();
 
   return (
     <Button
-      variant="outline"
+      variant="destructive"
       onClick={() => {
         toast({
           variant: "destructive",
-          title: "Oh não! Algo deu errado.",
-          description: "Ocorreu um problema com a sua requisição.",
+          title: "Deseja realmente excluir a tarefa?",
+
           action: (
-            <ToastAction
-              altText="Tentar novamente"
-              className="outline outline-1 outline-slate-100"
-            >
-              Reenviar
-            </ToastAction>
+            <>
+              <ToastAction
+                altText="Tentar novamente"
+                className="outline outline-1 outline-slate-100"
+              >
+                Confirmar
+              </ToastAction>
+              <ToastClose className="inline-flex h-8 shrink-0 items-center justify-center rounded-md border bg-slate-200 text-red-900 px-3 text-sm font-medium ring-offset-slate-100 transition-colors hover:bg-red-900 hover:text-slate-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none">
+                Cancelar
+              </ToastClose>
+              ,
+            </>
           ),
         });
       }}
     >
-      Mostrar Toast
+      Excluir
     </Button>
   );
 }
