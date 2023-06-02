372bfe1936e26c8ded10b748048757b2f4356eda
Flavio Alvarenga <flavio.alva@outlook.com>
2023-06-02 10:47:54 -0300

    feat(todo-id): add loading

--- src/app/todo/[todoId]/page.tsx
+++ src/app/todo/[todoId]/page.tsx
@@ -37,11 +37,14 @@ const TodoPage = ({ params }: PageProps) => {
   const [todo, setTodo] = useState<Todo>(defaultTodo);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const router = useRouter();
+  const date = !todo.done
+    ? getDateToLocale(todo.createdAt)
+    : getDateToLocale(todo.updatedAt);
 
   useEffect(() => {
     async function load() {
       const _todo = await getTodo(params.todoId);
-      console.log(_todo);
+      // console.log(_todo);
       setTodo(() => _todo);
       setIsLoading(() => false);
     }
@@ -75,39 +78,62 @@ const TodoPage = ({ params }: PageProps) => {
 
   return (
     <div className="text-center container flex flex-col items-center">
-      <Card className="w-3/4 p-4 my-4 dark:shadow-foreground/10 shadow-md ">
-        <CardHeader className="text-slate-400">TAREFA</CardHeader>
-        <CardContent className="text-4xl">
-          <span>{todo.title}</span>
-        </CardContent>
+      {isLoading ? (
+        <>
+          <Card className="w-3/4 p-4 my-4 dark:shadow-foreground/10 shadow-md ">
+            <CardHeader className="text-slate-400">TAREFA</CardHeader>
+            <CardContent className="text-4xl">
+              <p>Carregando...</p>
+            </CardContent>
 
-        <CardDescription className="text-xl">
-          <span>{todo.done ? `Completa` : `Incompleta`}</span>
-        </CardDescription>
+            <CardDescription className="text-xl">
+              <p>Carregando...</p>
+            </CardDescription>
 
-        <CardFooter className="text-center text-lg text-slate-500">
-          <span className="min-w-full">
-            {todo.done
-              ? `Finalizada em ${getDateToLocale(todo.updatedAt)}`
-              : `Criada em ${getDateToLocale(todo.createdAt)}`}
-          </span>
-        </CardFooter>
-      </Card>
+            <CardFooter className="text-center text-lg text-slate-500">
+              <p className="min-w-full">Carregando...</p>
+            </CardFooter>
+          </Card>
+        </>
+      ) : (
+        <>
+          <Card className="w-3/4 p-4 my-4 dark:shadow-foreground/10 shadow-md ">
+            <CardHeader className="text-slate-400">TAREFA</CardHeader>
+            <CardContent className="text-4xl">
+              <p>{todo.title}</p>
+            </CardContent>
 
-      <div className="text-4xl w-3/4 font-bold my-2 flex justify-between gap-2">
-        <Link href="/" className={buttonVariants({ variant: "outline" })}>
-          Voltar
-        </Link>
+            <CardDescription className="text-xl">
+              <p>{todo.done ? `Completa` : `Incompleta`}</p>
+            </CardDescription>
 
-        <div className="flex gap-x-2">
-          <Button variant={"default"}>Editar</Button>
+            <CardFooter className="text-center text-lg text-slate-500">
+              <p className="min-w-full">
+                {todo.done ? `Finalizada em ${date}` : `Criada em ${date}`}
+              </p>
+            </CardFooter>
+          </Card>
 
-          <DeleteTodoBtn handleDelete={handleDelete}>Excluir</DeleteTodoBtn>
-          {/* <Button variant={"destructive"} onClick={handleDelete}>
-            Excluir
-          </Button> */}
-        </div>
-      </div>
+          <div className="text-4xl w-3/4 font-bold my-2 flex justify-between gap-2">
+            <Link href="/" className={buttonVariants({ variant: "outline" })}>
+              Voltar
+            </Link>
+
+            <div className="flex gap-x-2">
+              <Button variant={"default"} disabled={isLoading ?? true}>
+                Editar
+              </Button>
+
+              <DeleteTodoBtn
+                handleDelete={handleDelete}
+                disabled={isLoading ?? true}
+              >
+                Excluir
+              </DeleteTodoBtn>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
