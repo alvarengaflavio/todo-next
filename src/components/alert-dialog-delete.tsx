9346a41406961c6eb2410571f98e3e5f02397d1d
Flavio Alvarenga <flavio.alva@outlook.com>
2023-06-06 11:26:33 -0300

    feat(components): create alert-dialog component

---
+++ src/components/alert-dialog-delete.tsx
@@ -0,0 +1,37 @@
+import { FC } from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+interface AlertDialogProps {}
+
+const AlertDialogDelete: FC<AlertDialogProps> = ({}) => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger>Open</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Tem certeza?</AlertDialogTitle>
+          <AlertDialogDescription>
+            Você está prestes a excluir uma tarefa. Essa ação não pode ser
+            desfeita. Tem certeza que deseja continuar?
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancelar</AlertDialogCancel>
+          <AlertDialogAction>Excluir</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default { AlertDialogDelete };
