be1b3aedc001e0a9181345ec55ced89ff16e7114
Flavio Alvarenga <flavio.alva@outlook.com>
2023-06-07 09:55:35 -0300

    feat(next-auth): set signin authorize credentials

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,5 @@
+import { prisma } from "@/lib/db/db";
+import { compare } from "bcrypt";
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
@@ -17,9 +19,32 @@ export const authOptions: NextAuthOptions = {
         password: { label: "Senha", type: "password" },
       },
       async authorize(credentials) {
-        // Handle Auth
-        const user = { id: "1", name: "Flávio", email: "test@test.com" };
-        return user;
+        if (!credentials?.email || !credentials?.password) return null;
+
+        const response = await fetch("http://localhost:3000/api/auth/login", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(credentials),
+        });
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user?.email) return null;
+
+        const isPasswordValid = await compare(
+          credentials.password,
+          user.password
+        );
+
+        if (!isPasswordValid) return null;
+
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+        };
       },
     }),
   ],
