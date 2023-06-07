afbc5797f463215ef33e7822c8b097da28515c85
Flavio Alvarenga <flavio.alva@outlook.com>
2023-06-07 09:27:11 -0300

    feat(prisma): create seed file

---
+++ prisma/seed.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from "@prisma/client";
+import { hash } from "bcrypt";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const password = await hash("test", 12);
+  const user = await prisma.user.upsert({
+    where: { email: "test@test.com" },
+    update: {},
+    create: {
+      email: "test@test.com",
+      name: "Test User",
+      password,
+    },
+  });
+  console.log(user);
+}
+
+main()
+  .then(() => {
+    prisma.$disconnect();
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
