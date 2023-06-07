0ec678b525fbf7f39e1b7f38742b4ef2a0c3fa72
Flavio Alvarenga <flavio.alva@outlook.com>
2023-06-07 09:00:39 -0300

    feat(user): create user.tsx

---
+++ src/config/user.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { stat } from "fs/promises";
+import { useSession } from "next-auth/react";
+
+export const User = () => {
+  const { data: session, status } = useSession();
+
+  return (
+    <>
+      <pre>{JSON.stringify(session, null, 2)}</pre>
+      <pre>{JSON.stringify(status, null, 2)}</pre>
+    </>
+  );
+};
