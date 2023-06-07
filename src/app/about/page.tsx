ccf796b3978710cd0695c9b413479dbe0aaee2e6
Flavio Alvarenga <flavio.alva@outlook.com>
2023-06-07 08:06:06 -0300

    feat(about): test session info

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,18 @@
 import { ToastDestructive } from "@/components/toast-destructive";
 import { ToastSimple } from "@/components/toast-simple";
 import { ToastWithAction } from "@/components/toast-with-action";
+import { getServerSession } from "next-auth";
 import { FC } from "react";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
 interface PageProps {}
 
-const About: FC<PageProps> = ({}) => {
+const AboutPage = async ({}) => {
+  const session = await getServerSession(authOptions);
+
   return (
     <section>
+      <pre>{JSON.stringify(session)}</pre>
       <ToastSimple />
       <ToastWithAction />
       <ToastDestructive />
@@ -15,4 +20,4 @@ const About: FC<PageProps> = ({}) => {
   );
 };
 
-export default About;
+export default AboutPage;
