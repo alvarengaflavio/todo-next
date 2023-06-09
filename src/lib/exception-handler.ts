fbd3ed83c6286de892bda15231199bbb80cf4979
Flavio Alvarenga <flavio.alva@outlook.com>
2023-06-09 07:17:45 -0300

    feat(lib): add `AuthRequiredException` handler

--- src/lib/exception-handler.ts
+++ src/lib/exception-handler.ts
@@ -1,20 +1,32 @@
 import { Prisma } from "@prisma/client";
 import { z } from "zod";
+import { AuthRequiredException } from "./exceptions";
 
 export function exceptionHandler(error: any) {
+  if (error instanceof AuthRequiredException) {
+    const errObj = buildAuthErrorObject(error);
+
+    return new Response(JSON.stringify(errObj), {
+      status: 403,
+      statusText: errObj.name ?? "Forbidden",
+    });
+  }
+
+  if (error instanceof z.ZodError) {
+    const errObj = buildZodErrorObject(error);
+
+    return new Response(JSON.stringify(errObj), {
+      status: errObj.code ?? 400,
+      statusText: errObj.name ?? "Unprocessable Content",
+    });
+  }
+
   if (error instanceof Prisma.PrismaClientValidationError) {
     const errObj = buildPrismaErrorObject(error);
 
     return new Response(JSON.stringify(errObj), {
       status: errObj.code ?? 400,
       statusText: errObj.name ?? "Bad Request",
-    });
-  } else if (error instanceof z.ZodError) {
-    const errObj = buildZodErrorObject(error);
-
-    return new Response(JSON.stringify(errObj), {
-      status: errObj.code ?? 400,
-      statusText: errObj.name ?? "Unprocessable Content",
     });
   }
 
@@ -70,6 +82,14 @@ const buildZodErrorObject = (error: z.ZodError): ErrorObject => {
     name: "Unprocessable Content",
     code: 422,
     message: error.issues[0]?.message ?? "Unprocessable Content",
+  };
+};
+
+const buildAuthErrorObject = (error: AuthRequiredException): ErrorObject => {
+  return {
+    name: "Forbidden Error",
+    code: 403,
+    message: "Forbidden - " + error.message ?? "Forbidden",
   };
 };
 
