bbb382429a825b57a365221eedfa2ca94e2b22f9
Flavio Alvarenga <flavio.alva@outlook.com>
2023-06-09 11:05:19 -0300

    feat(nav-user): add `useSession` hook

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { CreditCard, LogOut, PlusCircle, Settings, User } from "lucide-react";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
@@ -12,14 +14,32 @@ import {
   DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { signIn, useSession } from "next-auth/react";
+import { Skeleton } from "./ui/skeleton";
 
 export function UserNav() {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return <Skeleton className="h-8 w-8 rounded-full " />;
+  }
+
+  if (status === "unauthenticated") {
+    return (
+      <Button variant="outline" onClick={() => signIn()}>
+        Entrar
+      </Button>
+    );
+  }
+
+  console.log(session);
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
-            <AvatarImage src="/avatars/01.png" alt="@shadcn" />
+            <AvatarImage src="/avatars/01.png" alt="Avatar do usuário" />
             <AvatarFallback>SC</AvatarFallback>
           </Avatar>
         </Button>
@@ -27,9 +47,11 @@ export function UserNav() {
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">shadcn</p>
+            <p className="text-sm font-medium leading-none">
+              {session?.user?.name ?? "usuário"}
+            </p>
             <p className="text-xs leading-none text-muted-foreground">
-              m@example.com
+              {session?.user?.email ?? "email"}
             </p>
           </div>
         </DropdownMenuLabel>
@@ -37,7 +59,7 @@ export function UserNav() {
         <DropdownMenuGroup>
           <DropdownMenuItem>
             <User className="mr-2 h-4 w-4" />
-            <span>Profile</span>
+            <span>Perfil</span>
             <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
           </DropdownMenuItem>
           <DropdownMenuItem>
@@ -47,7 +69,7 @@ export function UserNav() {
           </DropdownMenuItem>
           <DropdownMenuItem>
             <Settings className="mr-2 h-4 w-4" />
-            <span>Settings</span>
+            <span>Configuração</span>
             <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
           </DropdownMenuItem>
           <DropdownMenuItem>
@@ -58,7 +80,7 @@ export function UserNav() {
         <DropdownMenuSeparator />
         <DropdownMenuItem>
           <LogOut className="mr-2 h-4 w-4" />
-          <span>Log out</span>
+          <span>Sair</span>
           <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
         </DropdownMenuItem>
       </DropdownMenuContent>
