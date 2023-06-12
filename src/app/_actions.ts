7ac6f695b8b864a49b997e182185b1dddcd9e955
alvalenda <banysan@msn.com>
2023-06-12 13:55:00 -0300

    feat(actions): add `createUserAction`

--- src/app/_actions.ts
+++ src/app/_actions.ts
@@ -2,9 +2,10 @@
 
 import { authOptions } from "@/lib/auth";
 import { prisma } from "@/lib/db/db";
-import { createTodoSchema } from "@/lib/zod";
+import { createTodoSchema, userAuthSchema, userCreateSchema } from "@/lib/zod";
 import { Todo } from "@/types";
 import { Prisma } from "@prisma/client";
+import { hash } from "bcrypt";
 import { Session } from "next-auth";
 import { getServerSession } from "next-auth/next";
 
@@ -29,6 +30,26 @@ export async function createTodoAction(todo: Pick<Todo, "title">) {
   // * daqui o action deveria chamar o arquivo que lida com o prisma com await e depois revalidate em revalidatePatch("/")
 
   return await prisma.todo.create({ data });
+}
+
+export async function createUserAction(user: {
+  email: string;
+  name: string;
+  password: string;
+  confirmPassword: string;
+}) {
+  try {
+    const { email, name, password } = userCreateSchema.parse(user);
+    const data: Prisma.UserCreateInput = {
+      email,
+      name,
+      password: await hash(password, 12),
+    };
+
+    return await prisma.user.create({ data });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 interface ISession extends Session {
