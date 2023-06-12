f015561a9cdca4f82f9d173e8c1498caa15368c8
alvalenda <banysan@msn.com>
2023-06-12 15:06:46 -0300

    feat(api): fix root GET import

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,10 +1,9 @@
+import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
-import { authOptions } from "./auth/[...nextauth]/route";
 
 export async function GET(request: NextRequest) {
   const session = await getServerSession(authOptions);
-  console.log(session);
 
   return NextResponse.json({ authenticated: !!session?.user });
 }
