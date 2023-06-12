79d0895fb43578737d2d779d87db2e28d9c753ff
alvalenda <banysan@msn.com>
2023-06-12 11:44:02 -0300

    feat(lib): remove restrictions to test

--- src/lib/zod.ts
+++ src/lib/zod.ts
@@ -14,26 +14,26 @@ export const createTodoSchema = z.object({
     }),
 });
 
-export const createUserSchema = z.object({
+export const userAuthSchema = z.object({
   email: z.string().email({
-    message: "O email deve ser válido.",
+    message: "O email deve ser válido",
   }),
   password: z
     .string()
-    .min(6, {
-      message: "A senha deve ter no mínimo 6 caracteres.",
+    .min(4, {
+      message: "A senha deve ter no mínimo 6 caracteres",
     })
     .max(45, {
-      message: "A senha deve ter no máximo 45 caracteres.",
+      message: "A senha deve ter no máximo 45 caracteres",
     })
-    .regex(
-      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{6,}$/,
-      {
-        message:
-          "A senha deve conter uma letra maiúscula, uma minúscula, um número e um caractere especial.",
-      }
-    )
+    // .regex(
+    //   /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{4,}$/,
+    //   {
+    //     message:
+    //       "A senha deve conter uma letra maiúscula, uma minúscula, um número e um caractere especial",
+    //   }
+    // )
     .nonempty({
-      message: "A senha não pode ser vazia.",
+      message: "A senha não pode ser vazia",
     }),
 });
