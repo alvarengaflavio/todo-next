4299ee2e09aabdc5e9fa7fd8cdcbb3c94fdc4d91
alvalenda <banysan@msn.com>
2023-06-14 08:54:46 -0300

    feat(user): add `preferences` page

---
+++ src/app/(user)/preferences/page.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface PreferencesPageProps {}
+
+const PreferencesPage: FC<PreferencesPageProps> = ({}) => {
+  return <div>PreferencesPage</div>;
+};
+
+export default PreferencesPage;
