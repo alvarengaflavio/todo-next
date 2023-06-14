721b9a088086543afb5e5ce4234e3bfc8511fdf1
alvalenda <banysan@msn.com>
2023-06-14 07:25:40 -0300

    feat(todo): refactor `todos` folder structure

---
+++ src/app/(todo)/todos/page.tsx
@@ -0,0 +1,16 @@
+import { CreateTodoForm } from "@/components/todo-create";
+import { TodoList } from "@/components/todo-list";
+
+export default async function Home() {
+  return (
+    <main className="flex min-h-screen container flex-col items-center justify-between px-24 py-4">
+      <section className="w-3/4">
+        <div className="mx-2 my-2 bg-slate-100 rounded-sm dark:bg-transparent grid grid-cols-1 gap-4 p-5">
+          <h1 className="text-5xl text-center font-light"> TODO LIST</h1>
+          <CreateTodoForm />
+          <TodoList />
+        </div>
+      </section>
+    </main>
+  );
+}
