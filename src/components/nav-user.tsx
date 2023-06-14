c735522377258636d84507d355d6e962793dcd5b
alvalenda <banysan@msn.com>
2023-06-14 08:53:59 -0300

    feat(nav-user): set `dropdown-menu-item` names

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -75,7 +75,7 @@ export function UserNav() {
 
           <DropdownMenuItem>
             <CreditCard className="mr-2 h-4 w-4" />
-            <span>Billing</span>
+            <span>Pagamento</span>
             <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
           </DropdownMenuItem>
 
