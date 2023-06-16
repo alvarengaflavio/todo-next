2f7eb61ec67a0aff22289741a5eb7f529e867431
alvalenda <banysan@msn.com>
2023-06-16 14:14:07 -0300

    chore(app): remove `typography` plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -55,6 +55,7 @@ module.exports = {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+
       keyframes: {
         "accordion-down": {
           from: { height: 0 },
