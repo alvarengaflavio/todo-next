9393a9afa3f3416389b91acf0dc2e1b160dc0432
alvalenda <banysan@msn.com>
2023-06-19 09:41:19 -0300

    feat(account): init page

--- src/app/(user)/account/page.tsx
+++ src/app/(user)/account/page.tsx
@@ -3,7 +3,11 @@ import { FC } from "react";
 interface AccountPageProps {}
 
 const AccountPage: FC<AccountPageProps> = ({}) => {
-  return <div>AccountPage</div>;
+  return (
+    <section className="w-full text-center">
+      <h2 className="text-2xl font-semibold mt-4">Conta</h2>
+    </section>
+  );
 };
 
 export default AccountPage;
