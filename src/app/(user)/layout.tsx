f8bcdee6673ef13ae81f126883b6b34ad91eae2f
alvalenda <banysan@msn.com>
2023-06-19 10:36:03 -0300

    feat(user): layout add `responsivity`

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -14,7 +14,9 @@ const UserLayout = async ({ children }: UserLayoutProps) => {
     <div className="flex min-h-screen flex-col space-y-6">
       <SiteHeader />
       <div className="container flex-1">
-        <main className="flex-1 sm:h-[45rem] overflow-hidden">{children}</main>
+        <main className="flex-1 h-[40rem] md:h-[45rem] overflow-hidden">
+          {children}
+        </main>
       </div>
       <SiteFooter className="border-t" />
     </div>
