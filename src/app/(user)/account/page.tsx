8e56af90eb4dda2071e5088786b083d255b26f46
alvalenda <banysan@msn.com>
2023-06-19 10:04:47 -0300

    feat(account): add `tabs` ui

--- src/app/(user)/account/page.tsx
+++ src/app/(user)/account/page.tsx
@@ -17,55 +17,56 @@ interface AccountPageProps {}
 const AccountPage: FC<AccountPageProps> = ({}) => {
   return (
     <section className="w-full h-full text-center flex flex-col items-center">
-      <h2 className="text-2xl font-semibold my-6">CONTA</h2>
+      <h2 className="text-2xl font-thin my-6">OPÇÕES DE CONTA</h2>
       <Tabs defaultValue="account" className="w-[400px]">
         <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="account">Account</TabsTrigger>
-          <TabsTrigger value="password">Password</TabsTrigger>
+          <TabsTrigger value="account">Conta</TabsTrigger>
+          <TabsTrigger value="password">Senha</TabsTrigger>
         </TabsList>
         <TabsContent value="account">
           <Card>
             <CardHeader>
-              <CardTitle>Account</CardTitle>
+              <CardTitle>Conta</CardTitle>
               <CardDescription>
-                Make changes to your account here. Click save when you're done.
+                Faça alterações na sua conta aqui. Clique em salvar quando
+                terminar.
               </CardDescription>
             </CardHeader>
             <CardContent className="space-y-2">
               <div className="space-y-1">
-                <Label htmlFor="name">Name</Label>
+                <Label htmlFor="name">Nome</Label>
                 <Input id="name" defaultValue="Pedro Duarte" />
               </div>
               <div className="space-y-1">
-                <Label htmlFor="username">Username</Label>
+                <Label htmlFor="username">Nome de Usuário</Label>
                 <Input id="username" defaultValue="@peduarte" />
               </div>
             </CardContent>
             <CardFooter>
-              <Button>Save changes</Button>
+              <Button>Salvar Mudanças</Button>
             </CardFooter>
           </Card>
         </TabsContent>
         <TabsContent value="password">
           <Card>
             <CardHeader>
-              <CardTitle>Password</CardTitle>
+              <CardTitle>Senha</CardTitle>
               <CardDescription>
-                Change your password here. After saving, you'll be logged out.
+                Altere sua senha aqui. Após salvar, você será desconectado.
               </CardDescription>
             </CardHeader>
             <CardContent className="space-y-2">
               <div className="space-y-1">
-                <Label htmlFor="current">Current password</Label>
+                <Label htmlFor="current">Senha atual</Label>
                 <Input id="current" type="password" />
               </div>
               <div className="space-y-1">
-                <Label htmlFor="new">New password</Label>
+                <Label htmlFor="new">Nova senha</Label>
                 <Input id="new" type="password" />
               </div>
             </CardContent>
             <CardFooter>
-              <Button>Save password</Button>
+              <Button>Salvar Senha</Button>
             </CardFooter>
           </Card>
         </TabsContent>
