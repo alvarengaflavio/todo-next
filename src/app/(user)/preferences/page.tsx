8eceffdcf398a4f83da7e712867912c2c5cfe58b
alvalenda <banysan@msn.com>
2023-06-19 11:53:04 -0300

    feat(preferences): add base `preferences` page

--- src/app/(user)/preferences/page.tsx
+++ src/app/(user)/preferences/page.tsx
@@ -3,7 +3,11 @@ import { FC } from "react";
 interface PreferencesPageProps {}
 
 const PreferencesPage: FC<PreferencesPageProps> = ({}) => {
-  return <div>PreferencesPage</div>;
+  return (
+    <section className="w-full h-full text-center flex flex-col items-center">
+      <h2 className="text-2xl font-thin my-6">PREFERÊNCIAS DE CONTA</h2>
+    </section>
+  );
 };
 
 export default PreferencesPage;
