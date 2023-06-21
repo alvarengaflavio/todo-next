91a7f6bb3eaa69c10041a24fbc7594aca4685f09
alvalenda <banysan@msn.com>
2023-06-21 08:15:58 -0300

    feat(preferences): dynamic read `avatars` folder

--- src/app/(user)/preferences/page.tsx
+++ src/app/(user)/preferences/page.tsx
@@ -1,11 +1,59 @@
 import { FC } from "react";
+import Image from "next/image";
 
 interface PreferencesPageProps {}
 
-const PreferencesPage: FC<PreferencesPageProps> = ({}) => {
+import fs from "fs";
+import path from "path";
+
+// Caminho absoluto para a pasta public
+const publicFolderPath = path.join(process.cwd(), "public");
+
+// Caminho absoluto para a pasta avatars
+const avatarsFolderPath = path.join(publicFolderPath, "avatars");
+
+// Função para ler os arquivos dentro da pasta avatars
+const readAvatarFiles = async () => {
+  try {
+    const avatarFiles = await fs.promises.readdir(avatarsFolderPath);
+    console.log(avatarFiles);
+    // Faça algo com os nomes dos arquivos aqui
+    return avatarFiles;
+  } catch (error) {
+    console.error("Erro ao ler os arquivos da pasta avatars:", error);
+  }
+};
+
+readAvatarFiles();
+
+const PreferencesPage = async ({}: PreferencesPageProps) => {
+  const avatars = await readAvatarFiles();
+
+  if (!avatars) {
+    return null;
+  }
+
   return (
     <section className="w-full h-full text-center flex flex-col items-center">
       <h2 className="text-2xl font-thin my-6">PREFERÊNCIAS DE CONTA</h2>
+
+      {avatars.map((avatar, i) => {
+        return (
+          <div
+            className="flex flex-col items-center"
+            key={`${i + "" + avatar}`}
+          >
+            <Image
+              // className="w-32 h-32 rounded-full"
+              src={`/avatars/${avatar}`}
+              alt={`avatar ${avatar}`}
+              width="40"
+              height="40"
+            />
+            <p className="text-xl font-semibold">{avatar}</p>
+          </div>
+        );
+      })}
     </section>
   );
 };
