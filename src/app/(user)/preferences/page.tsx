255b22f98bc1f0eb74035d837a359786c65f20d6
alvalenda <banysan@msn.com>
2023-06-21 10:16:25 -0300

    refactor(preferences): remove comments

--- src/app/(user)/preferences/page.tsx
+++ src/app/(user)/preferences/page.tsx
@@ -2,19 +2,10 @@ import fs from "fs";
 import path from "path";
 import { AvatarToggle } from "./avatar-toggle";
 
-interface PreferencesPageProps {
-  params: {
-    url?: string;
-    query?: string;
-  };
-}
-// Caminho absoluto para a pasta public
+interface PreferencesPageProps {}
 const publicFolderPath = path.join(process.cwd(), "public");
-
-// Caminho absoluto para a pasta avatars
 const avatarsFolderPath = path.join(publicFolderPath, "avatars");
 
-// Função para ler os arquivos dentro da pasta avatars
 const readAvatarFiles = async () => {
   try {
     const avatarFiles = await fs.promises.readdir(avatarsFolderPath);
@@ -25,9 +16,7 @@ const readAvatarFiles = async () => {
   }
 };
 
-readAvatarFiles();
-
-const PreferencesPage = async ({ params }: PreferencesPageProps) => {
+const PreferencesPage = async ({}: PreferencesPageProps) => {
   const avatars = await readAvatarFiles();
 
   if (!avatars) {
@@ -38,9 +27,7 @@ const PreferencesPage = async ({ params }: PreferencesPageProps) => {
     <section className="w-full h-full text-center flex flex-col items-center">
       <h2 className="text-2xl font-thin my-6">PREFERÊNCIAS DE CONTA</h2>
 
-      <div className="flex items-center space-x-4 border border-1 p-8 rounded-md">
-        <AvatarToggle avatars={avatars} />
-      </div>
+      <AvatarToggle avatars={avatars} />
     </section>
   );
 };
