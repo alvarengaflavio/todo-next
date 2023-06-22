6f574536acbc7a53da6ef1eba2ef7dab8023724d
alvalenda <banysan@msn.com>
2023-06-22 09:43:40 -0300

    refactor: remove createTodoAction !IMPORTANT

--- src/app/_actions.ts
+++ src/app/_actions.ts
@@ -1,36 +1,10 @@
 "use server";
 
-import { authOptions } from "@/lib/auth";
 import { prisma } from "@/lib/db/db";
-import { createTodoSchema, userCreateSchema } from "@/lib/zod";
-import { Todo } from "@/types";
+import { userCreateSchema } from "@/lib/zod";
 import { Prisma } from "@prisma/client";
 import { hash } from "bcrypt";
 import { Session } from "next-auth";
-import { getServerSession } from "next-auth/next";
-
-export async function createTodoAction(todo: Pick<Todo, "title">) {
-  const schema = createTodoSchema;
-  const body = schema.parse(todo);
-  const session: ISession | null = await getServerSession(authOptions);
-
-  if (!session) {
-    return new Response("Unauthorized", { status: 403 });
-  }
-
-  const { user } = session;
-  if (!user) return new Response("Unauthorized", { status: 403 });
-
-  const id = user.id ? { id: user.id } : undefined;
-  const data: Prisma.TodoCreateInput = {
-    title: body.title,
-    user: { connect: id },
-  };
-
-  // * daqui o action deveria chamar o arquivo que lida com o prisma com await e depois revalidate em revalidatePatch("/")
-
-  return await prisma.todo.create({ data });
-}
 
 export async function createUserAction(user: {
   email: string;
