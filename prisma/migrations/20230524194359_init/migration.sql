6a9645e5823d609e36f746512f92f91add20d7be
alvalenda <banysan@msn.com>
2023-06-24 08:11:44 -0300

    refactor(db): add `username` to user

--- prisma/migrations/20230524194359_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "todo" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "done" BOOLEAN NOT NULL DEFAULT false,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" DATETIME NOT NULL
-);
