f6fb613b0b8cc39658018b698e0958900ab4dfaf
alvalenda <banysan@msn.com>
2023-06-26 11:19:22 -0300

    fix(session): remove `ISession` type

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,11 +1,11 @@
-import { ISession } from "@/types";
+"use client";
 import { getServerSession } from "next-auth/next";
-
 import { authOptions } from "@/lib/auth";
+import { Session } from "next-auth";
 
 export async function getCurrentUser() {
   const session = await getServerSession(authOptions);
-  const _session = session as ISession;
+  const _session = session as Session;
 
   return _session?.user;
 }
