bfa21b39ff587cc9df2cb77042556d9217e3351c
alvalenda <banysan@msn.com>
2023-06-26 11:04:16 -0300

    fix(account-tabs): fix `imports`

--- src/app/(user)/account/account-tabs.tsx
+++ src/app/(user)/account/account-tabs.tsx
@@ -19,7 +19,6 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { toast } from "@/components/ui/use-toast";
 import { userUpdatePasswordSchema, userUpdateSchema } from "@/lib/zod";
