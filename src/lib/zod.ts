fd75de2e52739ecef04a0fb70be526ffc1c41982
alvalenda <banysan@msn.com>
2023-06-27 12:52:24 -0300

    refactor(zod): `createTodoSchema` error messages

--- src/lib/zod.ts
+++ src/lib/zod.ts
@@ -4,13 +4,13 @@ export const createTodoSchema = z.object({
   title: z
     .string()
     .min(6, {
-      message: "A tarefa deve ter no mínimo 6 caracteres.",
+      message: "deve ter no mínimo 6 caracteres.",
     })
     .max(45, {
-      message: "A tarefa deve ter no máximo 45 caracteres.",
+      message: "deve ter no máximo 45 caracteres.",
     })
     .nonempty({
-      message: "A tarefa não pode ser vazia.",
+      message: "a tarefa não pode ser vazia.",
     }),
 });
 
