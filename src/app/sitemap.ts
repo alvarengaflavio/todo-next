6aeeae8f6aaf16d66274014833f510b79314afea
alvalenda <banysan@msn.com>
2023-06-28 18:33:58 -0300

    feat(sitemap): add sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,19 +2,25 @@ import { Todo } from "@/types";
 
 export default async function sitemap() {
   const res = await fetch("http://jsonplaceholder.typicode.com/todos");
-  const allPosts = (await res.json()) as Todo[];
+  const allTodos = (await res.json()) as Todo[];
 
-  const posts = allPosts.map((post) => ({
-    url: `http://localhost:3000/dashboard/${post.id}`,
+  const todos = allTodos.map((todo) => ({
+    url: `http://localhost:3000/todo/${todo.id}`,
     lastModified: new Date().toISOString(),
   }));
 
-  const routes = ["", "/create", "/login", "/rendering", "/dashboard"].map(
-    (route) => ({
-      url: `http://localhost:3000${route}`,
-      lastModified: new Date().toISOString(),
-    })
-  );
+  const routes = [
+    "",
+    "/register",
+    "/login",
+    "/todos",
+    "/about",
+    "/account",
+    "/preferences",
+  ].map((route) => ({
+    url: `http://localhost:3000${route}`,
+    lastModified: new Date().toISOString(),
+  }));
 
-  return [...routes, ...posts];
+  return [...routes, ...todos];
 }
