a0e9c5be48af15bfe4900d3149797e19713e6899
alvalenda <banysan@msn.com>
2023-07-03 12:25:18 -0300

    style: add favicon

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -11,10 +11,7 @@ export const contentType = "image/png";
 export default function Icon() {
   return new ImageResponse(
     (
-      <div
-        tw="w-full h-full flex items-center justify-center text-md text-white bg-black font-bold rounded-md"
-        // className="flex items-center text-md text-white bg-black font-bold rounded-md"
-      >
+      <div tw="w-full h-full flex items-center justify-center text-md text-white bg-black font-bold rounded-md">
         Ta
       </div>
     ),
