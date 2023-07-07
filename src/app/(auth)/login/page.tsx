bf30ee4ee52fd3aece3af0021f83023cfcea3572
alvalenda <banysan@msn.com>
2023-07-07 09:39:56 -0300

    style: login page bg

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,7 +5,7 @@ interface PageProps {}
 
 const LoginPage: FC<PageProps> = () => {
   return (
-    <section className="w-4/5 h-full flex flex-col items-center justify-center">
+    <section className="w-4/5 h-full flex flex-col items-center justify-center bg-secondary">
       <UserAuthForm />
     </section>
   );
