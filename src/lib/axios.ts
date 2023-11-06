633c94d14b94f298045317a33a2b97bc2148ff9c
alvalenda <banysan@msn.com>
2023-11-06 17:45:36 -0300

    Add console logs and use vercel_url variable for baseURL

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,9 +1,16 @@
 import axios from "axios";
 
+const vercel_url = process.env.VERCEL_URL;
+const api_url = window.location.href;
+
+console.log("vercel_url", vercel_url);
+console.log("api_url", api_url);
+
 const api = axios.create({
   baseURL:
     process.env.NEXT_PUBLIC_API_URL ||
-    `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`,
+    // `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`,
+    (vercel_url as string),
 
   headers: {
     "Content-Type": "application/json",
