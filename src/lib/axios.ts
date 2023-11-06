6e88b6b00c37f2b62135dd87ee687207362e304c
alvalenda <banysan@msn.com>
2023-11-06 18:04:43 -0300

    Update API URL in axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,20 +1,18 @@
 import axios from "axios";
 
-const vercel_url = process.env.VERCEL_URL;
 const vercel_url2 = process.env.NEXT_PUBLIC_VERCEL_URL;
-
-console.log("vercel_url", vercel_url);
-console.log("vercel_url2", vercel_url2);
 
 const api = axios.create({
   baseURL:
-    process.env.NEXT_PUBLIC_API_URL || `https://${process.env.VERCEL_URL}/api`,
+    process.env.NEXT_PUBLIC_API_URL ||
+    `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}/api` ||
+    `https://${process.env.NEXT_PUBLIC_BRANCH_URL}/api`,
 
   headers: {
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods":
-      "GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH",
+    // "Access-Control-Allow-Origin": "*",
+    // "Access-Control-Allow-Methods":
+    //   "GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH",
   },
 });
 
