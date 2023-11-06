dfab8b53e8003e2809d33cfbac129eae5aba574a
alvalenda <banysan@msn.com>
2023-11-06 18:11:36 -0300

    Update API URL in axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,12 +1,10 @@
 import axios from "axios";
 
-const vercel_url2 = process.env.NEXT_PUBLIC_VERCEL_URL;
-
 const api = axios.create({
   baseURL:
     process.env.NEXT_PUBLIC_API_URL ||
     `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}/api` ||
-    `https://${process.env.NEXT_PUBLIC_BRANCH_URL}/api`,
+    `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`,
 
   headers: {
     "Content-Type": "application/json",
