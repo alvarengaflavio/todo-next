7d4e5b197c8e5ea6ad6fb51b62f4194a9af9b817
alvalenda <banysan@msn.com>
2023-11-06 17:47:52 -0300

    Update axios.ts to use NEXT_PUBLIC_VERCEL_URL environment variable.

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const vercel_url = process.env.VERCEL_URL;
-const api_url = window.location.href;
+const vercel_url2 = process.env.NEXT_PUBLIC_VERCEL_URL;
 
 console.log("vercel_url", vercel_url);
-console.log("api_url", api_url);
+console.log("vercel_url2", vercel_url2);
 
 const api = axios.create({
   baseURL:
