8b9757b45cf8aa3f9b4906b3608bfee2ec73417f
alvalenda <banysan@msn.com>
2023-11-06 17:08:07 -0300

    Update axios.ts to use NEXT_PUBLIC_VERCEL_URL as base URL

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,13 +1,10 @@
 import axios from "axios";
-import { headers } from "next/headers";
-
-const host = headers().get("host");
 
 const api = axios.create({
   baseURL:
     process.env.NEXT_PUBLIC_API_URL ||
-    //`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`,
-    `http://${host}/api`,
+    `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`,
+
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
