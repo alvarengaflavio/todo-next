4db3bfe73a7b136151d24b123b5d40d29f56b9f4
alvalenda <banysan@msn.com>
2023-11-09 09:51:46 -0300

    Add className prop to AuthButtons component

--- src/components/user-auth.tsx
+++ src/components/user-auth.tsx
@@ -3,12 +3,15 @@
 import { FC } from "react";
 import { Button } from "./ui/button";
 import { signIn, signOut } from "next-auth/react";
+import { cn } from "@/lib/utils";
 
-interface AuthButtons {}
+interface AuthButtons {
+  className?: string;
+}
 
-const AuthButtons: FC<AuthButtons> = ({}) => {
+const AuthButtons: FC<AuthButtons> = ({ className }) => {
   return (
-    <div className="flex items-center space-x-2 my-2">
+    <div className={cn(className, " flex items-center space-x-2 my-2 ")}>
       <Button variant={"default"} onClick={() => signIn()}>
         Entrar
       </Button>
