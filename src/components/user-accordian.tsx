72732180756e2f9a93ff81254dc99e6a104272bc
alvalenda <banysan@msn.com>
2023-11-10 08:47:23 -0300

    Refactor `UserAccordion` component to include `children` prop

--- src/components/user-accordian.tsx
+++ src/components/user-accordian.tsx
@@ -10,15 +10,17 @@ import { cn } from "@/lib/utils";
 interface UserAccordionProps {
   className?: string;
   props?: React.ComponentPropsWithoutRef<"div">;
+  children?: React.ReactNode;
 }
 
-const UserAccordion: FC<UserAccordionProps> = ({ className, props }) => {
+const UserAccordion: FC<UserAccordionProps> = ({ className, children }) => {
   return (
     <Accordion type="single" className={cn(" ", className)} collapsible>
       <AccordionItem value="item-1">
         <AccordionTrigger>Is it accessible?</AccordionTrigger>
         <AccordionContent>
           Yes. It adheres to the WAI-ARIA design pattern.
+          {children}
         </AccordionContent>
       </AccordionItem>
     </Accordion>
