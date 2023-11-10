1716ca59ca9b338978d1da30e963f896fd18f20e
alvalenda <banysan@msn.com>
2023-11-10 08:58:08 -0300

    Update `UserAccordion` component to display `user` `authentication status`

--- src/components/user-accordian.tsx
+++ src/components/user-accordian.tsx
@@ -17,10 +17,18 @@ const UserAccordion: FC<UserAccordionProps> = ({ className, children }) => {
   return (
     <Accordion type="single" className={cn(" ", className)} collapsible>
       <AccordionItem value="item-1">
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionTrigger>Estou devidamente autenticado?</AccordionTrigger>
         <AccordionContent>
-          Yes. It adheres to the WAI-ARIA design pattern.
-          {children}
+          Abaixo segue o status da autenticação do usuário seguido dos dados da
+          seção do usuário. Caso o usuário não esteja autenticado, será recebido
+          o objeto{" "}
+          <code className="bg-slate-200 dark:bg-slate-800 px-0.5 rounded-sm">
+            null
+          </code>
+          .
+          <br />
+          <br /> {children}
+          <br />
         </AccordionContent>
       </AccordionItem>
     </Accordion>
