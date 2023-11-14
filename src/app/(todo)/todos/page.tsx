18df0132e830731bf9692d3f9850f2a348a41d61
alvalenda <banysan@msn.com>
2023-11-14 17:55:31 -0300

    Update page layout and section width in Todo app

--- src/app/(todo)/todos/page.tsx
+++ src/app/(todo)/todos/page.tsx
@@ -3,8 +3,8 @@ import { TodoList } from "@/components/todo-list";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen container flex-col items-center justify-between px-24 py-4">
-      <section className="w-3/4">
+    <main className="flex min-h-screen container flex-col items-center justify-between px-2 lg:px-10 2xl:px24 py-4">
+      <section className="w-full xl:w-3/4">
         <div className="mx-2 my-2 bg-slate-100 rounded-sm dark:bg-transparent grid grid-cols-1 gap-4 p-5">
           <h1 className="text-5xl text-center font-light"> TODO LIST</h1>
           <CreateTodoForm />
