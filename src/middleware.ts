0d6bbe74b75b9ff7b49ca40d3663910ceda7678c
alvalenda <banysan@msn.com>
2023-11-21 07:49:11 -0300

    Update middleware.ts with additional routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/todos", "/todo/:path*"],
+  matcher: ["/todos", "/todo/:path*", "/pricing", "/account", "/preferences"],
 };
