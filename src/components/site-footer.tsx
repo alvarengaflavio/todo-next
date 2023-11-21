704de2233cf39ad306c0c1f0362bd6738bd604c2
alvalenda <banysan@msn.com>
2023-11-21 07:52:13 -0300

    Update site footer text

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -14,16 +14,16 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
         <div className="flex flex-col items-center px-8 md:flex-row md:gap-2 md:px-0">
           <Icons.logo />
           <p className="text-center text-sm leading-loose md:text-left">
-            {currentYear}. Built by{" "}
+            {currentYear}. Construído por{" "}
             <a
               href={siteConfig.links.twitter}
               target="_blank"
               rel="noreferrer"
               className="font-medium underline underline-offset-4"
             >
-              alva
+              Flávio Alvarenga
             </a>
-            . The source code is available on{" "}
+            . Código fonte disponível no{" "}
             <a
               href={siteConfig.links.github}
               target="_blank"
